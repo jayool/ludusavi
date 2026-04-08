8e683fef79a7ae2c59ac578e4a6b85af027155e3
jayool <111197667+jayool@users.noreply.github.com>
2026-04-08 15:45:34 +0200

    Update button.rs

--- src/gui/button.rs
+++ src/gui/button.rs
@@ -325,10 +325,10 @@ pub fn nav<'a>(screen: Screen, current_screen: Screen) -> Button<'a> {
         Screen::Restore => TRANSLATOR.nav_restore_button(),
         Screen::CustomGames => TRANSLATOR.nav_custom_games_button(),
         Screen::Other => TRANSLATOR.nav_other_button(),
-        Screen::Games | Screen::ThisDevice | Screen::AllDevices => return Button::new(
+        Screen::Games | Screen::ThisDevice | Screen::AllDevices | Screen::GameDetail(_) => return Button::new(
             text("").size(14).align_x(alignment::Horizontal::Center)
         )
-        .on_press(Message::SwitchScreen(screen))
+        .on_press(Message::SwitchScreen(Screen::Games))
         .padding([5, 20])
         .class(style::Button::NavButtonInactive),
     };
