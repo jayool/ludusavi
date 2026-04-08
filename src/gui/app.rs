837f41b97b1084f68f2e19314208fe042e7a4cf5
jayool <111197667+jayool@users.noreply.github.com>
2026-04-08 21:57:25 +0200

    Update app.rs

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -3196,7 +3196,6 @@ impl App {
                     &self.text_histories,
                     self.config.scan.show_deselected_games,
                     vec![],
-                    true,
                 );
                 
                 let table_header = Row::new()
