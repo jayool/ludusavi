6e61775a14bc7c1a1f43213ea8fa23ac3cfedf42
jayool <111197667+jayool@users.noreply.github.com>
2026-04-22 12:02:14 +0200

    Update cache.rs

--- src/resource/cache.rs
+++ src/resource/cache.rs
@@ -123,18 +123,18 @@ impl Cache {
         }
     }
 
-    pub fn has_root(&self, candidate: &Root) -> bool {
-        self.roots.iter().any(|root| {
-            let primary = root.path().equivalent(candidate.path()) && root.store() == candidate.store();
-            match (root, candidate) {
-                (Root::Lutris(root), Root::Lutris(candidate)) => {
-                    primary && (root.database.is_some() || candidate.database.is_none())
+        pub fn has_root(&self, candidate: &Root) -> bool {
+            self.roots.iter().any(|root| {
+                let primary = root.path().equivalent(candidate.path()) && root.store() == candidate.store();
+                match (root, candidate) {
+                    (Root::Lutris(root), Root::Lutris(candidate)) => {
+                        primary && (root.database.is_some() || candidate.database.is_none())
+                    }
+                    _ => primary,
                 }
-                _ => primary,
-            }
-        })
+            })
+        }
     }
-}
 
     pub fn should_check_app_update(&self) -> bool {
         let now = chrono::offset::Utc::now();
